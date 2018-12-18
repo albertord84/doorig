5a56899ab58d760ed1878b51bb6b6701bdf043e3
josergm86 <josergm86@gmail.com>
2018-12-18 08:19:53 -0200

    a

--- src/application/views/responsive_views/admin/users_end_painel.php
+++
@@ -1,6 +0,0 @@
-
-    <br>
-    <img width="15%" src="<?php echo base_url().'assets/images/dumbu logo 2 png.png'?>"> <br>
-    <img width="15%" src="<?php echo base_url().'assets/images/direitos.png'?>">        
-    <br>
-    <br>
