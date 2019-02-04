a48b3ec6843574312437329e2d73fc225f83ca53
Alberto Reyes <albertord84@gmail.com>
2019-02-04 15:27:41 -0200

    Step1 OK

---
+++ src/application/business/Client.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace business {
+
+  //require_once config_item('business-user-class');
+
+  /**
+   * @category Business class
+   * 
+   * @access public
+   *
+   * @todo Define an Client business class.
+   * 
+   */
+  class Client extends Bussines {
+
+    /**
+     * 
+     * @access public
+     * @var type
+     *  
+     */
+    public $Proxy_id;
+    public $Name;
+    public $Email;
+    public $Password;
+    public $Status_id;
+    public $Node_id;
+    public $Phone;
+    public $Verification_code;
+    public $Init_date;
+    public $Last_access;
+    public $Utm_source;
+    public $Utm_campain;
+    public $Login_token;
+
+    /**
+     * 
+     * @todo Class constructor.
+     * 
+     */
+    function __construct() {
+      parent::__construct();
+
+      $ci = &get_instance();
+      $ci->load->model('clients_model');
+      
+      require_once getcwd() . '/application/business/Business.php';
+    }
+
+    /**
+     * Get client data
+     * @param int $client_id
+     * @return DataSet  
+     */
+    public function load_data(int $id) {
+      $ci = &get_instance();
+      $data = $ci->Clients_model->get_by_id($id);
+
+      $this->fill_data($data);
+    }
+
+    /**
+     * Get client data
+     * @param int $client_id
+     * @return DataSet  
+     */
+    public function load_data_by_email(string $email) {
+      $ci = &get_instance();
+      $data = $ci->Clients_model->get_by_email($id);
+
+      $this->fill_data($data);
+    }
+
+    private function fill_data(\stdClass $data) {
+      $this->Name = $data->name;
+      $this->Email = $data->email;
+      $this->Password = $data->password;
+      $this->Status_id = $data->status_id;
+      $this->Node_id = $data->node_id;
+      $this->Phone = $data->phone;
+      $this->Verification_code = $data->verification_code;
+      $this->Init_date = $data->init_date;
+      $this->Last_access = $data->last_access;
+      $this->Utm_source = $data->utm_source;
+      $this->Utm_campain = $data->utm_campain;
+      $this->Login_token = $data->login_token;
+    }
+
+    /**
+     * 
+     * @todo
+     * @param type
+     * @return
+     * 
+     */
+    public function set_client_status($id = NULL, $status_id = NULL) {
+      $ci = &get_instance();
+      try {
+        $id = $id ? $id : $this->id;
+        $status_id = $status_id ? $status_id : $this->status_id;
+        $result = $ci->Clients_model->update($id, NULL, NULL, NULL, $status_id);
+      } catch (Exception $exc) {
+        echo $exc->getTraceAsString();
+      }
+    }
+
+    /**
+     * 
+     * @return type
+     */
+    public function get_clients() {
+      $ci = &get_instance();
+      return $ci->Clients_model->get_all();
+    }
+
+    function check_pass($password) {
+      if ($this->Password === $password) {
+        return TRUE;
+      }
+      return FALSE;
+    }
+
+  }
+
+}
+?>
--- src/application/controllers/Signin.php
+++ src/application/controllers/Signin.php
@@ -4,6 +4,9 @@ ini_set('xdebug.var_display_max_depth', 256);
 ini_set('xdebug.var_display_max_children', 256);
 ini_set('xdebug.var_display_max_data', 1024);
 
+use business\Response\Response;
+use business\Client;
+
 class Signin extends CI_Controller {
 
   public function __construct() {
@@ -12,10 +15,11 @@ class Signin extends CI_Controller {
     // TODO: Preguntar a Carlos
     //require_once config_item('business-reponse-class');
     require_once getcwd() . '/application/business/Response/Response.php';
+    require_once getcwd() . '/application/business/Client.php';
   }
 
   //----------LOGIN FUNCTIONS--------------------------
-  //OJO: Decidir si se va a hacer un controlador para el login y recuperacion de senha
+
   public function login_view() {
     $this->load->view('login');
   }
@@ -35,44 +39,60 @@ class Signin extends CI_Controller {
   }
 
   public function do_login() {
-    $datas = $this->input->post();
-    //implementar aqui el resto		
-    //$this->redirect_to_dasboard();
+    try {
+      $datas = $this->input->post();
+
+      $Client = new Client();
+      $Client->load_data_by_email($datas["email"]);
+      if ($Client->check_pass($datas["password"]) == FALSE)
+        throw new Exception("Password do not match for this email");
+    } catch (Exception $exc) {
+      Response::ResponseFAIL($exc->getTraceAsString())->toJson();
+    }
+
+    Response::ResponseOK()->toJson();
   }
 
+  //---------------SIGNIN FUNCTIONS-----------------------------
+  // Step 1 {
   public function signin_step1() {
-    //$this->load->library('input');
-    $datas = $this->input->post("name");
-    //1. Validate Signin Data
-    //$this->load->library('form_validation');
-    //$this->form_validation->set_rules();
-    //$this->form_validation->set_rules('name', 'Name', 'required');
-    //$this->form_validation->set_rules('email', 'Email', 'required');
-    //$this->form_validation->set_rules('phone', 'Phone', 'required');
-    //$this->form_validation->set_rules('password', 'Password', 'required');
-    //$this->form_validation->set_rules('password-rep', 'Password Confirmation', 'required');
-    //if ($this->form_validation->run() == FALSE) {
-    // Returnd erros
-    // $this->form_validation->validation_errors();
-    //echo json_encode($response);
-    //return;
-    //}
-    //2. Save Signin Data
-    $datas["name"] = "777";
-    $datas["email"] = "777@7.7";
-    $datas["phone"] = "777 77";
-    $datas["password"] = "7777";
-    $datas["password-rep"] = "7777";
-    $datas["status_id"] = "1";
-    $datas["node_id"] = "1";
-    $this->load->model('Clients_model');
-    $client_id = $this->Clients_model->save($datas["name"], $datas["email"], $datas["password"], $datas["status_id"], $datas["node_id"], $datas["phone"]);
+    try {
 
+
+      //$this->load->library('input');
+      $datas = $this->input->post();
+      //1. Validate Signin Data
+      //$this->load->library('form_validation');
+      //$this->form_validation->set_rules();
+      //$this->form_validation->set_rules('name', 'Name', 'required');
+      //$this->form_validation->set_rules('email', 'Email', 'required');
+      //$this->form_validation->set_rules('phone', 'Phone', 'required');
+      //$this->form_validation->set_rules('password', 'Password', 'required');
+      //$this->form_validation->set_rules('password-rep', 'Password Confirmation', 'required');
+      //if ($this->form_validation->run() == FALSE) {
+      // Returnd erros
+      // $this->form_validation->validation_errors();
+      //echo json_encode($response);
+      //return;
+      //}
+      //2. Save Signin Data
+      $datas["name"] = "777";
+      $datas["email"] = "777@7.7";
+      $datas["phone"] = "777 77";
+      $datas["password"] = "7777";
+      $datas["password-rep"] = "7777";
+      $datas["status_id"] = "1";
+      $datas["node_id"] = "1";
+      $this->load->model('Clients_model');
+      $client_id = $this->Clients_model->save($datas["name"], $datas["email"], $datas["password"], $datas["status_id"], $datas["node_id"], $datas["phone"]);
+    } catch (Exception $exc) {
+      Response::ResponseFAIL(1, $exc->getTraceAsString())->toJson();
+    }
     // Retur Response   
-    $reponse = $client_id ? new business\Response\Response(0, "Steep 1 OK") : new business\Response\Response(1, "Error inserting client");
-    echo $reponse->toJson();
+    Response::ResponseOK()->toJson();
   }
 
+  // Step 2 {
   public function request_secure_code_by_email() {
     //implementar aqui el resto
     //retornar success ou error
@@ -85,6 +105,7 @@ class Signin extends CI_Controller {
     echo json_encode($response);
   }
 
+  // Step 3 {
   public function confirm_secure_code() {
     $datas = $this->input->post();
     //implementar aqui el resto		
