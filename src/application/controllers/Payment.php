6524d30e42053658a815f106bee4ec8c058082bb
Alberto Reyes <albertord84@gmail.com>
2019-03-21 10:51:46 -0300

    Alberto Payment

--- src/application/controllers/Payment.php
+++ src/application/controllers/Payment.php
@@ -9,7 +9,7 @@ use business\Response\Response;
 use business\Response\ResponseArrayObject;
 use business\Visitor;
 
-class Welcome extends CI_Controller {
+class Payment extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
@@ -23,7 +23,15 @@ class Welcome extends CI_Controller {
 
     public function vindi_notification_post() {
         $datas = $this->input->post();
-        var_dump($datas);
+        //$post_str = urldecode($_POST['post_str']);
+        //$post = urldecode($post_str);
+        //$post = json_decode($post);
+        $post = 'Alberto Test: Thanks GOD!!!';
+        // Write the contents back to the file
+        $path = __dir__ . '/../../../logs/vindi/';
+        $file = $path . "vindi_notif_post-" . date("d-m-Y") . ".log";
+        $result = file_put_contents($file, "\n\n", FILE_APPEND);
+        $result = file_put_contents($file, serialize($post), FILE_APPEND);
         try {
             
         } catch (Exception $e) {
